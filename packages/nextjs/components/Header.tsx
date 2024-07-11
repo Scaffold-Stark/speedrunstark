79200fed2f39da359ff7cd82369545de74422adc
gianmalarcon <gian@quantum3labs.com>
2024-07-12 00:07:43 +0700

    Fix: fmt

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -5,7 +5,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import {
-  ArrowDownTrayIcon,  ArrowPathIcon,
+  ArrowDownTrayIcon,
+  ArrowPathIcon,
   ArrowUpTrayIcon,
   Bars3Icon,
   BugAntIcon,
