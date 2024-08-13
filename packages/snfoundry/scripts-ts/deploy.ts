7695cd9efe13b3ccbe971141dfaf2df216924e95
Nadai2010 <nadai2010@proton.me>
2024-08-13 20:18:41 +0100

    Update Challenge-token-vendor last Version Scaffold Stark

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -1,60 +1,62 @@
 import {
   deployContract,
   executeDeployCalls,
-  exportDeployments,
   deployer,
+  exportDeployments,
 } from "./deploy-contract";
 import { green } from "./helpers/colorize-log";
 
-/**
- * Deploy a contract using the specified parameters.
- *
- * @example (deploy contract with contructorArgs)
- * const deployScript = async (): Promise<void> => {
- *   await deployContract(
- *     {
- *       contract: "YourContract",
- *       contractName: "YourContractExportName",
- *       constructorArgs: {
- *         owner: deployer.address,
- *       },
- *       options: {
- *         maxFee: BigInt(1000000000000)
- *       }
- *     }
- *   );
- * };
- *
- * @example (deploy contract without contructorArgs)
- * const deployScript = async (): Promise<void> => {
- *   await deployContract(
- *     {
- *       contract: "YourContract",
- *       contractName: "YourContractExportName",
- *       options: {
- *         maxFee: BigInt(1000000000000)
- *       }
- *     }
- *   );
- * };
- *
- *
- * @returns {Promise<void>}
- */
+let your_token: any;
+let vendor: any;
 const deployScript = async (): Promise<void> => {
-  await deployContract({
-    contract: "YourContract",
+  your_token = await deployContract({
+    contract: "YourToken",
     constructorArgs: {
-      owner: deployer.address,
+      recipient: deployer.address, // ~~~YOUR FRONTEND ADDRESS HERE~~~~
     },
   });
 };
 
-deployScript()
-  .then(async () => {
-    await executeDeployCalls();
-    exportDeployments();
+// Todo: Uncomment Vendor deploy lines
+// vendor = await deployContract({
+//  contract: "Vendor",
+//  constructorArgs: {
+//   eth_token_address:
+//    "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+//  your_token_address: your_token.address,
 
-    console.log(green("All Setup Done"));
+// Todo: Add owner address, should be the same as `deployer.address`
+// },
+// });
+
+// const transferScript = async (): Promise<void> => {
+//transfer 1000 GLD tokens to VendorContract
+//  await deployer.execute(
+//    [
+//      {
+//       contractAddress: your_token.address,
+//     calldata: [
+//     vendor.address,
+//   {
+//       low: 1_000_000_000_000_000_000_000n, //1000 * 10^18
+//       high: 0,
+//        },
+//      ],
+//     entrypoint: "transfer",
+//   },
+// ],
+// {
+//  maxFee: 1e18,
+//    }
+// );
+// };
+
+deployScript()
+  .then(() => {
+    executeDeployCalls().then(() => {
+      exportDeployments();
+      // transferScript();
+    });
+    console.log("All Setup Done");
   })
   .catch(console.error);
