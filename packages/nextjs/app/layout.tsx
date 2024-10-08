fc43d2004a3cc5753f4c8c908c3f7c48a399ecd4
Nadai2010 <nadai2010@proton.me>
2024-10-08 21:50:14 +0100

    Fixed UI and name in manifest

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -2,9 +2,11 @@ import type { Metadata } from "next";
 import { ScaffoldStarkAppWithProviders } from "~~/components/ScaffoldStarkAppWithProviders";
 import "~~/styles/globals.css";
 import { ThemeProvider } from "~~/components/ThemeProvider";
+import { Space_Grotesk } from "next/font/google";
+const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Scaffold-Stark",
+  title: "Speedrun Stark",
   description: "Fast track your starknet journey",
   icons: "/logo.ico",
 };
@@ -12,7 +14,7 @@ export const metadata: Metadata = {
 const ScaffoldStarkApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
-      <body>
+      <body className={spaceGrotesk.className}>
         <ThemeProvider enableSystem>
           <ScaffoldStarkAppWithProviders>
             {children}
