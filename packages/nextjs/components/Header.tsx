c77124bdcdf1b9f568513cea9a8ea3006af4a0ba
danielcdz <calde0601@gmail.com>
2024-10-09 22:39:04 -0600

    Update Header to V3

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -4,21 +4,14 @@ import React, { useCallback, useRef, useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import {
-  ArrowDownTrayIcon,
-  ArrowPathIcon,
-  ArrowUpTrayIcon,
-  Bars3Icon,
-  BugAntIcon,
-  PhotoIcon,
-} from "@heroicons/react/24/outline";
+import { Bars3Icon, BugAntIcon, PhotoIcon } from "@heroicons/react/24/outline";
 import { useOutsideClick } from "~~/hooks/scaffold-stark";
 import { CustomConnectButton } from "~~/components/scaffold-stark/CustomConnectButton";
 import { useTheme } from "next-themes";
 import { useTargetNetwork } from "~~/hooks/scaffold-stark/useTargetNetwork";
 import { devnet } from "@starknet-react/chains";
 import { SwitchTheme } from "./SwitchTheme";
-import { useAccount, useProvider } from "@starknet-react/core";
+import { useAccount, useNetwork, useProvider } from "@starknet-react/core";
 
 type HeaderMenuLink = {
   label: string;
@@ -28,24 +21,9 @@ type HeaderMenuLink = {
 
 export const menuLinks: HeaderMenuLink[] = [
   {
-    label: "My NFTs",
-    href: "/myNFTs",
+    label: "Example View 1",
+    href: "/exampleView1",
     icon: <PhotoIcon className="h-4 w-4" />,
-  },
-  {
-    label: "Transfers",
-    href: "/transfers",
-    icon: <ArrowPathIcon className="h-4 w-4" />,
-  },
-  {
-    label: "IPFS Upload",
-    href: "/ipfsUpload",
-    icon: <ArrowUpTrayIcon className="h-4 w-4" />,
-  },
-  {
-    label: "IPFS Download",
-    href: "/ipfsDownload",
-    icon: <ArrowDownTrayIcon className="h-4 w-4" />,
   },
   {
     label: "Debug Contracts",
@@ -98,21 +76,42 @@ export const Header = () => {
     useCallback(() => setIsDrawerOpen(false), []),
   );
   const { targetNetwork } = useTargetNetwork();
-  const isLocalNetwork = targetNetwork.id === devnet.id;
+  const isLocalNetwork = targetNetwork.network === devnet.network;
 
   const { provider } = useProvider();
-  const { address, status } = useAccount();
+  const { address, status, chainId } = useAccount();
+  const { chain } = useNetwork();
   const [isDeployed, setIsDeployed] = useState(true);
 
   useEffect(() => {
-    if (status === "connected" && address) {
-      provider.getContractVersion(address).catch((e) => {
-        if (e.toString().includes("Contract not found")) {
-          setIsDeployed(false);
-        }
-      });
+    if (
+      status === "connected" &&
+      address &&
+      chainId === targetNetwork.id &&
+      chain.network === targetNetwork.network
+    ) {
+      provider
+        .getClassHashAt(address)
+        .then((classHash) => {
+          if (classHash) setIsDeployed(true);
+          else setIsDeployed(false);
+        })
+        .catch((e) => {
+          console.error("contreact cehc", e);
+          if (e.toString().includes("Contract not found")) {
+            setIsDeployed(false);
+          }
+        });
     }
-  }, [status, address, provider]);
+  }, [
+    status,
+    address,
+    provider,
+    chainId,
+    targetNetwork.id,
+    targetNetwork.network,
+    chain.network,
+  ]);
 
   return (
     <div className="sticky lg:static top-0 navbar min-h-0 flex-shrink-0 justify-between z-20 px-0 sm:px-2">
