1266b820da8a05562e02faa706efc617499fe696
Buuh2511 <buuhuynh2511@gmail.com>
2025-01-22 18:34:58 +0700

    clean up prev component

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -1,44 +1,19 @@
-import React, { useCallback, useRef, useState } from "react";
-import { BugAntIcon } from "@heroicons/react/24/outline";
-import { useOutsideClick } from "~~/hooks/scaffold-stark";
-import { usePathname } from "next/navigation";
-import { FaucetButton } from "~~/components/scaffold-stark/FaucetButton";
-import HeaderLogo from "./HeaderLogo";
+import React from "react";
 import { LanguageButton } from "./Language/LanguageButton";
 import { AccountButton } from "./Wallet/AccountInfor";
-
-type HeaderMenuLink = {
-  label: string;
-  href: string;
-  icon?: React.ReactNode;
-};
-
-export const menuLinks: HeaderMenuLink[] = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Debug Contracts",
-    href: "/debug",
-    icon: <BugAntIcon className="h-4 w-4" />,
-  },
-];
+import Image from "next/image";
 
 export const Header = () => {
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  const burgerMenuRef = useRef<HTMLDivElement>(null);
-  const pathname = usePathname();
-  useOutsideClick(
-    burgerMenuRef,
-    useCallback(() => setIsDrawerOpen(false), []),
-  );
-
   return (
-    <div className="bg-[#4D58FF] md:px-6 md:py-4 px-2 py-3 flex justify-between">
+    <div className="bg-[#4D58FF] md:px-6 md:py-4 px-2 py-3 flex justify-between relative z-50">
       <div>
         <div onClick={() => (window.location.href = "/")}>
-          <HeaderLogo />
+          <Image
+            src={"/homescreen/starknet-logo.svg"}
+            alt="logo"
+            width={120}
+            height={28}
+          />
         </div>
       </div>
       <div className="flex items-center md:gap-6 gap-2">
