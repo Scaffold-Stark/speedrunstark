acfe942e3bb737c6f0ca63450e3cf4b5ea5b2fae
Nadai2010 <nadai2010@proton.me>
2025-03-26 08:40:46 +0000

    Sync ch-3 with Scaffold

--- packages/nextjs/app/layout.tsx
+++ packages/nextjs/app/layout.tsx
@@ -2,11 +2,9 @@ import type { Metadata } from "next";
 import { ScaffoldStarkAppWithProviders } from "~~/components/ScaffoldStarkAppWithProviders";
 import "~~/styles/globals.css";
 import { ThemeProvider } from "~~/components/ThemeProvider";
-import { Space_Grotesk } from "next/font/google";
-const spaceGrotesk = Space_Grotesk({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Speedrun Stark",
+  title: "Scaffold-Stark",
   description: "Fast track your starknet journey",
   icons: "/logo.ico",
 };
@@ -14,7 +12,7 @@ export const metadata: Metadata = {
 const ScaffoldStarkApp = ({ children }: { children: React.ReactNode }) => {
   return (
     <html suppressHydrationWarning>
-      <body className={spaceGrotesk.className}>
+      <body>
         <ThemeProvider enableSystem>
           <ScaffoldStarkAppWithProviders>
             {children}
