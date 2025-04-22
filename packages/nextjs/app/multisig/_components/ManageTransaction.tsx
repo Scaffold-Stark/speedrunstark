b3a67c2ee976df9c893d7a44ec8d119e44fb42a8
Bao Ho Gia <54936754+BaoHG1508@users.noreply.github.com>
2025-04-22 13:20:16 +0700

    [chore] update readme

--- packages/nextjs/app/multisig/_components/ManageTransaction.tsx
+++ packages/nextjs/app/multisig/_components/ManageTransaction.tsx
@@ -138,7 +138,7 @@ export const ManageTransaction: React.FC<ManageTransactionProps> = ({
                   value={transferAmount || ""}
                   onChange={handleAmountChange}
                   placeholder="Enter amount in ETH"
-                  className="block w-full px-4 py-2 rounded-md bg-gray-700 border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  className="block w-full px-4 py-2 rounded-md border border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
               </div>
             </div>
