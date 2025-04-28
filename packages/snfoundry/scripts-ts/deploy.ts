65cf767e31403c664eeea3b5b214004a51816e1d
tu11aa <thanhtuhr01@gmail.com>
2025-04-28 13:55:53 +0700

    fix: replace ETH token address with STRK token address

--- packages/snfoundry/scripts-ts/deploy.ts
+++ packages/snfoundry/scripts-ts/deploy.ts
@@ -15,7 +15,7 @@ const deployScript = async (): Promise<void> => {
     contract: "DiceGame",
     constructorArgs: {
       strk_token_address:
-        "0x49D36570D4E46F48E99674BD3FCC84644DDD6B96F7C741B1562B82F9E004DC7",
+        "0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
     },
     options: {
       maxFee: BigInt(5000000000000),
