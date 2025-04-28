13e4405d09dc9d127bea51a264c3a6d908742c3f
tu11aa <thanhtuhr01@gmail.com>
2025-04-28 17:49:41 +0700

    fix: missing while merging

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,10 +1,9 @@
 "use client";
 
-import type { NextPage } from "next";
 import Image from "next/image";
 import { useAccount } from "@starknet-react/core";
 
-const Home: NextPage = () => {
+const Home = () => {
   const connectedAddress = useAccount();
   return (
     <>
@@ -56,41 +55,7 @@ const Home: NextPage = () => {
           </div>
         </div>
       </div>
-
-      <div className="bg-container flex-grow w-full mt-16 px-8 py-12">
-        <div className="flex justify-center items-center gap-12 flex-col sm:flex-row">
-          <div className="flex flex-col bg-base-100 relative text-[12px] px-10 py-10 text-center items-center max-w-xs rounded-3xl border border-gradient">
-            <div className="trapeze"></div>
-            <Image
-              src="/debug-icon.svg"
-              alt="icon"
-              width={26}
-              height={30}
-            ></Image>
-            <p>
-              Tinker with your smart contract using the{" "}
-              <Link href="/debug" passHref className="link">
-                Debug Contracts
-              </Link>{" "}
-              tab.
-            </p>
-          </div>
-          <div className="flex flex-col bg-base-100 relative text-[12px] px-10 py-10 text-center items-center max-w-xs rounded-3xl border border-gradient">
-            <div className="trapeze"></div>
-            <Image
-              src="/explorer-icon.svg"
-              alt="icon"
-              width={20}
-              height={32}
-            ></Image>
-            <p>
-              Play around with Multiwrite transactions using
-              useScaffoldMultiWrite() hook
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
