405a3ec4590084b2b6166f3fba1a06235f361e12
tu11aa <thanhtuhr01@gmail.com>
2025-04-29 19:20:00 +0700

    feat: add a script to remove unnecessary files

--- remove_test.sh
+++ remove_test.sh
@@ -46,6 +46,6 @@ git restore --worktree --source=HEAD --quiet -- "${files_to_process[@]}"
 
 # 3. Remove newly added files (which became untracked after step 1)
 #    This command ignores tracked files.
-git clean -fd"
+git clean -fd
 
 exit 0
